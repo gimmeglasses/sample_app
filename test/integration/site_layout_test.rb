d5b81a9694a1f29d65f7d89bf769c7258aad1bde
gimmeglasses <50077615+gimmeglasses@users.noreply.github.com>
2025-05-09 02:48:03 +0000

    Added assert_select for signup_path

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,10 +4,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path # => GET access to /static_pages/home 
     assert_template 'static_pages/home' # static_pages/home is displayed
-    assert_select "a[href=?]", root_path, count: 2 # there are two links to root_path
-    assert_select "a[href=?]", help_path # there is one link to help_path
-    assert_select "a[href=?]", about_path # there is one link to about_path
-    assert_select "a[href=?]", contact_path # there is one link to contact_path
-    assert_select "a[href=?]", signup_path # there is one link to signup_path
+    assert_select "a[href=?]", root_path, count: 2  # there are two links to root_path
+    assert_select "a[href=?]", help_path            # there is one link to help_path
+    assert_select "a[href=?]", about_path           # there is one link to about_path
+    assert_select "a[href=?]", contact_path         # there is one link to contact_path
+    assert_select "a[href=?]", signup_path          # there is one link to signup_path
   end
 end
