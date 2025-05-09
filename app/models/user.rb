fbc5f1c75e1dc56b8d983ee5c18db1abb9bcbcce
gimmeglasses <50077615+gimmeglasses@users.noreply.github.com>
2025-05-09 12:54:35 +0000

    changed in chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # "before_save" (コールバック) works before the record is saved to the database
+  # provided email address is downcased, thus no need of case_sensitive
+  before_save { self.email = email.downcase }
+  # ApplicationRecordクラスの継承によって、アクティブレコードを利用できる様になる
+  # Apply validations to the model
+  # Validates that the name attribute is present
+  validates :name,  presence: true, length: { maximum: 50 }
+  # Validates that the email attribute is present
+  # データベースでは文字列の上限を255としているので、それに合わせて255文字を上限にする
+  # 正規表現を使って、メールアドレスの形式を検証する
+  # 大文字小文字を区別しているので、case_sensitive: 
+  # Railsはこの場合、:uniquenessをtrueと判断する (emailのユニークネスを検証する)
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence:   true, length: { maximum: 255 },
+                    format:     { with: VALID_EMAIL_REGEX }, 
+                    #uniqueness: { case_sensitive: false }
+                    uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8}
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,89 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  # setup method is run before each test
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     # passwordは、has_secure_passwordメソッドによってDBに登録されない
+                     password: "foobarxxx", password_confirmation: "foobarxxx")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    # Update user.name to be blank
+    @user.name = "     "
+    # Expect the user to be invalid
+    assert_not @user.valid?
+  end
+
+  test " email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    # Set the name to a string of 51 characters (51 <- ERROR CASE)
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    # Set the email to a string of 244 characters brfore the @example.com domain (256 <- ERROR CASE)
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com 
+                          USER@foo.COM 
+                          A_US-ER@foo.bar.org
+                          first.last@foo.jp 
+                          alice+bob@baz.cn]
+    # Iterate through each valid address and set it to @user.email
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      # Check if the user is valid
+      # If failed, it will show the invalid address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com 
+                            user_at_foo.org 
+                            user.name@example.
+                            foo@bar_baz.com 
+                            foo@bar+baz.com
+                            foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      # Check if the user is invalid
+      # If failed, it will show the invalid address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    # .dupメソッドは、オブジェクトのコピーを作成する
+    duplicate_user = @user.dup
+    # @userをデータベースに保存
+    @user.save
+    # duplicate_user
+    assert_not duplicate_user.valid?, "#{duplicate_user.email.inspect} should be invalid"
+  end
+
+  test "password should be present (nonblank)" do
+    # Set the password to a string of 8 spaces
+    # passwordは、has_secure_passwordメソッドによってDBに登録されない
+    @user.password = @user.password_confirmation = " " * 8
+    assert_not @user.valid?, "#{@user.password.inspect} should be invalid"
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 7
+    assert_not @user.valid?, "#{@user.password.inspect} should be invalid"
+  end
+end
