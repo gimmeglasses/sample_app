96858e8b56a4563ccea8beb093ee2033469fa3ed
gimmeglasses <50077615+gimmeglasses@users.noreply.github.com>
2025-05-18 03:24:59 +0000

    finish #07

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(user)
+    # GravatarのURLを生成するために、ユーザーのメールアドレスをMD5ハッシュ化
+    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    # GravatarのURLを生成
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+
+    # 暗黙の戻り値(４章でも扱ったもの)
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
