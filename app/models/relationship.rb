f4eb822abe3132d039d11082500ab7ec61b1e802
gimmeglasses <50077615+gimmeglasses@users.noreply.github.com>
2025-06-12 14:42:38 +0000

    completed ch.14

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ApplicationRecord
+  # these belongs_to methods build associations between 
+  # the Relationship model and the User model
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
   validates :follower_id, presence: true
